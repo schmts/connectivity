4cd1e63dbc1181d9bc06fb1434616c065e34ccfc
Jackobi Rejeck <99176060+schmts@users.noreply.github.com>
2023-01-24 22:26:12 +0100

    Update app.py

--- connectivity/app.py
+++ connectivity/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def ping():
-    return 'Ping-a-ding ...'
+    return 'Pinging in the rain ...'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
